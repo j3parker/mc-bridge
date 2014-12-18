8df68eb7f99a8ad2439c7dc0d8eaf2ec5f7d3134
Jacob Parker <j3parker@csclub.uwaterloo.ca>
2014-12-17 23:08:29 -0500

    Adding some commands

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 mc-bridge
 =========
 
-Link IRC channel and Minecraft chat
+Link IRC channel and Minecraft chat. This code isn't good. It depends on [https://github.com/j3parker/wrap](this utility).
--- bridge.py
+++ bridge.py
@@ -4,6 +4,8 @@ import sys
 import threading
 import time
 
+infoCount = 0
+lock = threading.Lock()
 
 def follow(file):
 	file.seek(0, 2)
@@ -14,34 +16,68 @@ def follow(file):
 			continue
 		yield line
 
+def sendToIrc(msg):
+	irc = open(ircDir + '/in', 'w')
+	irc.write(msg + '\n')
+	irc.close()
+
 def minecraftToIrc():
+	global infoCount, lock
+
 	lines = follow(open(mcDir + '/out'))
-	regex = re.compile('\[\d+:\d+:\d+\] \[Server thread/INFO\]: <([^>]*)> (.*)$')
+	reInfo = re.compile('^\[\d+:\d+:\d+\] \[Server thread/INFO\]: (.*)$')
+	reChat = re.compile('^<([^>]*)> (.*)$')
 	for line in lines:
-		m = regex.search(line)
+		m = reInfo.search(line)
+		if not(m):
+			continue
+		msg = m.groups()[0]
+
+		# Handle "/list" output
+		lock.acquire()
+		try:
+			if infoCount > 0:
+				infoCount -= 1
+				sendToIrc(msg)
+				continue
+		finally:
+			lock.release()
+
+		# Handle chats
+		m = reInfo.search(msg)
 		if not(m):
 			continue
 		name = m.groups()[0]
 		text = m.groups()[1]
-		irc = open(ircDir + '/in', 'w')
-		irc.write('<' + name + '> ' + text + '\n')
-		irc.close()
+
+		sendToIrc('<' + name + '> ' + text)
 
 def ircToMinecraft():
+	global infoCount, lock
 	lines = follow(open(ircDir+ '/out'))
-	regex = re.compile('\d+-\d+-\d+ \d+:\d+ <([^>]+)> (.*)')
+	regex = re.compile('\d+-\d+-\d+ \d+:\d+ <([^>]+)> ([^\s]*)(\s?)(.*)')
 	for line in lines:
 		m = regex.search(line)
 		if not(m):
 			continue
 		name = m.groups()[0]
-		text = m.groups()[1]
+		cmd = m.groups()[1]
+		sp = m.groups()[2]
+		text = m.groups()[3]
 
 		if name == 'minecraft':
 			continue
 
 		mc = open(mcDir + '/in', 'w')
-		mc.write('/say ' + name + ': ' + text + '\n')
+		if cmd == 'playsound':
+			mc.write('/playsound ' + text + '\n')
+		elif cmd == 'list' and text == '':
+			lock.acquire()
+			infoCount += 3
+			lock.release()
+			mc.write('/list\n')
+		else:
+			mc.write('/say ' + name + ': ' + cmd + sp + text + '\n')
 		mc.close()
 
 		time.sleep(1) # what am i doinggggggggg weird behaviour hacks
